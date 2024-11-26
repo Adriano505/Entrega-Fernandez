94c2da64f911f666e256f9d7ee0faec233fa4b12
Adriano505 <themaxwell112@hotmail.com>
2024-11-26 20:06:19 -0300

    Modificaciones

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Entrega-Fernandez/', // Ruta base para GitHub Pages
   server: {
     hmr: {
       overlay: false,
     },
   },
-})
+});
 
