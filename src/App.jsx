98f242bf503d7a71fcee5fa7953b5f8527681b63
Adriano505 <themaxwell112@hotmail.com>
2024-11-28 18:50:00 -0300

    Se agregan modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import Banner from "./components/Banner/Banner";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import ExtraCards from "./components/ExtraCards/ExtraCards";
-
-const bannerImageTop = "/Entrega-Fernandez/assets/img/banner/banner.jpg";
-const bannerImageBottom = "/Entrega-Fernandez/assets/img/banner/footer1.jpg";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Banner image={bannerImageTop} />
-              <ItemListContainer greeting="La moda de hoy en día" />
-              <Banner image={bannerImageBottom} />
-              <ExtraCards />
-            </>
-          }
-        />
-
-        <Route
-          path="/category/:categoryId"
-          element={
-            <>
-              <Banner image={bannerImageTop} />
-              <ItemListContainer greeting="Filtrando por categoría" />
-              <Banner image={bannerImageBottom} />
-            </>
-          }
-        />
-
-        <Route path="/product/:itemId" element={<ItemDetailContainer />} />
-      </Routes>
-    </BrowserRouter>
+    <NavBar />
   );
 };
 
